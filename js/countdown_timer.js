b09c54d6e744eb733faef7c99e172b96dcb3f7aa
Aman Saddall <saddalla@hotmail.co.uk>
2018-05-09 20:22:29 +0100

    updated

--- js/countdown_timer.js
+++ js/countdown_timer.js
@@ -1,27 +1,24 @@
 $(document).ready(function () {
 
-    $('.btn-timer').click(function () {
-        $('.time').each(function () {
+    $('.time').each(function () {
+        var timeTd = $(this);
 
-            var timeTd = $(this);
+        var timer2 = $(this).text();
+        var interval = setInterval(function () {
 
-            var timer2 = $(this).text();
-            var interval = setInterval(function () {
+            var timer = timer2.split(':');
+            //by parsing integer, I avoid all extra string processing
+            var minutes = parseInt(timer[0], 10);
+            var seconds = parseInt(timer[1], 10);
+            --seconds;
+            minutes = (seconds < 0) ? --minutes : minutes;
+            if (minutes < 0) clearInterval(interval);
+            seconds = (seconds < 0) ? 59 : seconds;
+            seconds = (seconds < 10) ? '0' + seconds : seconds;
+            //minutes = (minutes < 10) ?  minutes : minutes;
+            timeTd.html(minutes + ':' + seconds);
+            timer2 = minutes + ':' + seconds;
+        }, 1000);
 
-                var timer = timer2.split(':');
-                //by parsing integer, I avoid all extra string processing
-                var minutes = parseInt(timer[0], 10);
-                var seconds = parseInt(timer[1], 10);
-                --seconds;
-                minutes = (seconds < 0) ? --minutes : minutes;
-                if (minutes < 0) clearInterval(interval);
-                seconds = (seconds < 0) ? 59 : seconds;
-                seconds = (seconds < 10) ? '0' + seconds : seconds;
-                //minutes = (minutes < 10) ?  minutes : minutes;
-                timeTd.html(minutes + ':' + seconds);
-                timer2 = minutes + ':' + seconds;
-            }, 1000);
-
-        });
     });
 });
